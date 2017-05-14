fd3da66a149eaad4dd48ebaa7b2677b09bcb26b5
JanLabro <jblabro24@gmail.com>
2017-05-14 17:02:00 +0100

    Upload of Assignment - Model.h

--- asteroids/src/model.h
+++ asteroids/src/model.h
@@ -5,21 +5,63 @@ struct point {
 typedef struct point coordinate_t;
 typedef struct point vector_t;
 
-/* Some insitial struct types if you want to usd them */
+/* Some insitial struct types if you want to use them */
 struct ship {
-    coordinate_t p;
+    coordinate_t p;	
+		coordinate_t p0;
+		coordinate_t p1;
+		coordinate_t p2;
     vector_t     v;
+		vector_t		 a;
+		int collision;
+		float heading;
+		float headingRads;
 };
+
+struct shield {
+		coordinate_t s;
+		int size;
+		int colour;
+		bool exists;
+		int quality;
+};
+
 
 /* initial struts for building linked lists */
 struct rock {
     coordinate_t p;
+		vector_t     v;
+		int size;
+		int collision;
     struct rock *next;
 };
 
-struct missile {
+typedef struct rock rock_t;
+
+typedef struct asteroids {
+		rock r;		
+		struct asteroids *next;
+} asteroids_t;
+
+
+typedef struct missile {
     coordinate_t p;
+		vector_t v;
+		int outOfBounds;
+		int collision;
     struct missile *next;
-};
+} missile_t;
+
 
 void physics(void);
+ship move_ship(ship, float);
+shield move_shield(ship, shield, float);
+ship wrap_around(ship);
+missile_t *initialise_missile_list();
+asteroids_t *initialise_asteroid_list();
+ship initialise_player();
+
+
+extern missile_t *active;
+extern asteroids_t *active_asteroid;
+
